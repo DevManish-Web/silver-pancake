0177a80e7e0646418c07b550a3599871d8e156f1
Manish Singh <manishsingh21x07@gmail.com>
2025-08-25 10:22:40 +0530

    adding files

---
+++ script.js
@@ -0,0 +1,39 @@
+const startstop = document.getElementById("startstop");
+const seconds = document.getElementById("seconds");
+const minutes = document.getElementById("minutes");
+const hours = document.getElementById("hours");
+const days = document.getElementById("days");
+
+const today = new Date();
+const todayAt12 = new Date();
+todayAt12.setHours(0, 0, 0, 0);
+const birthdate = new Date("09/13/2025");
+let totalSeconds = 0;
+if (todayAt12.getTime() === birthdate.getTime()) {
+  totalSeconds = 0;
+} else {
+  birthdate.setFullYear(today.getFullYear() );
+  totalSeconds = (birthdate - today) / 1000;
+}
+
+const setDisplay = (sec) => {
+  days.innerText = String(Math.floor(sec / (24 * 60 * 60))).padStart(2, "0");
+  sec = sec % (24 * 60 * 60);
+  hours.innerText = String(Math.floor(sec / (60 * 60))).padStart(2, "0");
+  sec = sec % (60 * 60);
+  minutes.innerText = String(Math.floor(sec / 60)).padStart(2, "0");
+  sec = sec % 60;
+  seconds.innerText = String(Math.floor(sec)).padStart(2, "0");
+};
+
+const timer = setInterval(() => {
+  if (totalSeconds <= 0) {
+    totalSeconds = 0;
+    clearInterval(timer);
+    return;
+  }
+  totalSeconds -= 1;
+  setDisplay(totalSeconds);
+}, 1000);
+
+document.addEventListener('contextmenu', event => event.preventDefault());
